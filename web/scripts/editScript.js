13ade2cd78e5e1362547fe65d00995f7e215ea91
KubaBoi <jakubanderle@outlook.cz>
2023-03-17 11:38:45 +0100

    tab

--- web/scripts/editScript.js
+++ web/scripts/editScript.js
@@ -9,15 +9,51 @@ async function sourceEdit() {
             {"name": "id", "value": "source"},
             {"name": "class", "value": "main"}
         ]);
-
         let response = await callEndpoint("GET", mdUrl);
         if (response.ERROR == null) {
-            createElement("textarea", sourceDiv, response, [
+            let sourceInput = createElement("textarea", sourceDiv, response, [
                 {"name": "id", "value": "sourceInput"},
                 {"name": "class", "value": "sourceInput"},
                 {"name": "columns", "value": "500"},
                 {"name": "rows", "value": "500"}
             ]);
+            sourceInput.focus();
+            sourceInput.addEventListener('keydown', e => {
+                if (e.shiftKey && e.keyCode == 9) {
+                    e.preventDefault();
+                    let start = sourceInput.selectionStart;
+                    let end = sourceInput.selectionEnd;
+                    let value = sourceInput.value;
+                    let selection = value.slice(start, end);
+                    let lines = selection.split("\n");
+                    for (let i = 0; i < lines.length; i++) 
+                        for (let o = 0; o < 4; o++)
+                            if (lines[i][0] == " ")
+                                lines[i] = lines[i].slice(1);
+                    
+                    sourceInput.value = value.slice(0, start) + lines.join("\n") + value.slice(end);
+                    sourceInput.selectionStart = start - 4;
+                    sourceInput.selectionEnd = end - 4;
+                }
+                else if (e.keyCode == 9) {
+                    e.preventDefault();
+                    let start = sourceInput.selectionStart;
+                    let end = sourceInput.selectionEnd;
+                    let value = sourceInput.value;
+                    let selection = value.slice(start, end);
+                    let lines = selection.split("\n");
+                    for (let i = 0; i < lines.length; i++) 
+                        lines[i] = "    " + lines[i];
+                    
+                    sourceInput.value = value.slice(0, start) + lines.join("\n") + value.slice(end);
+                    sourceInput.selectionStart = start + 4;
+                    sourceInput.selectionEnd = end + (4 * (lines.length));
+                }
+            });
+        }
+        else {
+            sourceDiv.remove();
+            alert(`Error ${response.ERROR}`);
         }
     }
     else {
@@ -27,13 +63,13 @@ async function sourceEdit() {
 }
 
 async function save() {
-    let sourceDiv = document.getElementById("sourceInput");
+    let sourceInput = document.getElementById("sourceInput");
 
-    if (sourceDiv == null) {
+    if (sourceInput == null) {
         return;
     }
 
-    let content = sourceDiv.value;
+    let content = sourceInput.value;
     let request = {
         "FILE": mdUrl,
         "CONTENT": content
@@ -51,7 +87,7 @@ async function save() {
         getContents(dirPath);
         let resp= await callEndpoint("GET", mdUrl);
         if (resp.ERROR == null) 
-            sourceDiv.value = resp;
+            sourceInput.value = resp;
         else
             alert("Error, do not save!! Close edit and open it again.");
     }
@@ -59,9 +95,11 @@ async function save() {
 
 document.addEventListener('keydown', e => {
     if (e.ctrlKey && e.key === 's') {
-        // Prevent the Save dialog to open
         e.preventDefault();
-        // Place your code here
         save();
+    }
+    if (e.ctrlKey && e.key === 'x') {
+        e.preventDefault();
+        sourceEdit();
     }
 });
